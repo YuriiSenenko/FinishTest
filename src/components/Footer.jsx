c7b0d5557c81d20ac0afeed8b112777a8f5bdccc
Yurii Senenko <yurkosenenko@gmail.com>
2023-03-30 18:22:22 +0300

    fix nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ const Footer = () => {
   return (
     <footer>
       <nav>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
       </nav>
     </footer>
   );
